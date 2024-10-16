5f8dedf74c5f51a22ad50b581a25b340fd94c2ec
geyer-rayane <157837384+geyer-rayane@users.noreply.github.com>
2024-10-16 22:29:42 +0200

    Final change on pom.xml and test of IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,12 +1,32 @@
 package fr.univavignon.pokedex.api;
 import fr.univavignon.pokedex.api.*;
 
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class IPokedexFactoryTest {
 
-	
-	public IPokedexFactoryTest() {
-		
-		
+    private IPokedexFactory pokedexFactory;
+    private IPokemonMetadataProvider metadataProvider;
+    private IPokemonFactory pokemonFactory;
+    private IPokedex pokedex;
+
+    @Before
+    public void setUp() {
+        metadataProvider = mock(IPokemonMetadataProvider.class);
+        pokemonFactory = mock(IPokemonFactory.class);
 	}
 
+    @Test
+    public void testCreatePokedex() {
+        pokedex = mock(IPokedex.class);
+        when(pokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(pokedex);
+        IPokedex createdPokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        assertNotNull(createdPokedex);
+        assertEquals(pokedex, createdPokedex);
+        verify(pokedexFactory).createPokedex(metadataProvider, pokemonFactory);
+    }
 }
