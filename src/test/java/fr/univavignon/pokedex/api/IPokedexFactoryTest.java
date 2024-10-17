753aab868e03be1b1e646a51794ae07bb83c435e
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-10-17 10:24:26 +0200

    Add import test

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,12 +1,22 @@
 package fr.univavignon.pokedex.api;
 import fr.univavignon.pokedex.api.*;
 
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Arrays;
+
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class IPokedexFactoryTest {
 
