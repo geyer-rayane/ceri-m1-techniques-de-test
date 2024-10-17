7e4acd20bc0c7c171f73a53f881514f4fb55addf
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-10-17 11:28:25 +0200

    New try

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -18,26 +18,28 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class IPokemonMetadataProviderTest {
+    
     @Mock
     private IPokemonMetadataProvider metadataProvider;
 
     private PokemonMetadata pokemonMetadata;
 
     @Before
-    public void setUp() {
+    public void setup() {
         metadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
-        pokemonMetadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 0.56);
+        pokemonMetadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
     }
 
     @Test
     public void testGetPokemonMetadata() throws PokedexException {
-        if (!(0 <= pokemonMetadata.getIndex() && pokemonMetadata.getIndex() <= 150)) {
-            throw new PokedexException("Invalid ID. ID must be between 0 and 150");
-        }
-
         when(metadataProvider.getPokemonMetadata(0)).thenReturn(pokemonMetadata);
-        PokemonMetadata result = metadataProvider.getPokemonMetadata(0);
-        assertEquals(pokemonMetadata, result);
+        PokemonMetadata testPokemon=iPokemonMetadataProvider.getPokemonMetadata(0);
+        assertNotNull(testPokemon);
+        assertEquals(0, testPokemon.getInde));
+        assertEquals(90, testPokemon.getStamina());
+        assertEquals("Bulbizarre", testPokemon.getName());
+        assertEquals(126, testPokemon.getAttack());
+        assertEquals(126, testPokemon.getDefense());
     }
-
 }
+        
