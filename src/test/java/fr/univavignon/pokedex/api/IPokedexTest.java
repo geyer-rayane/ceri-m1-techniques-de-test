b65b643cfb50446c993cc5c5d5bbef39236f8acc
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-10-17 09:28:26 +0200

    Correction of error

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -2,15 +2,12 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 import static org.junit.Assert.*;
 
