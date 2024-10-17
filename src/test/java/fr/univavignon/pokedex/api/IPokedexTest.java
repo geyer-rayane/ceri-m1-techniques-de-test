64ee71532fc6a595dd75247910ac99eddd385ae1
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-10-17 09:55:11 +0200

    Add Arrays

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -11,6 +11,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Arrays;
+
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
