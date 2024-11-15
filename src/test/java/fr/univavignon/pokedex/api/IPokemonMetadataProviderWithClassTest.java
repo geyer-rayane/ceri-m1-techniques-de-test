c8abbff7fcf69516d1a1576416b98db710528ce7
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-11-15 11:20:05 +0100

    Maj

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderWithClassTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderWithClassTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.*;
 
 public class IPokemonMetadataProviderWithClassTest {
     
-    private PokemonMetadataProvider metadataProvider;
+    private PokemonMetaDataProvider metadataProvider;
     private PokemonMetadata pokemonMetadata;
 
     @Before
