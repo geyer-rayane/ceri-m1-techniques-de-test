817bb9c8cb0d1cc3a9652531201b65f145c477c5
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-11-15 10:07:24 +0100

    Modif PokemonFactory => iv determination and creation

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -2,12 +2,18 @@ package fr.univavignon.pokedex.api;
 
 import java.util.Comparator;
 import java.util.List;
+import Random ; 
 
 public class PokemonFactory implements IPokemonFactory {
 
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-        Pokemon pokemon = new Pokemon(index, "pokemon", 0, 0, 0, cp, hp, dust, candy, 0);
-        return pokemon ; // a revoir pour le nom par exemple..
+
+        PokemonMetaDataProvider metadataProvider = new PokemonMetaDataProvider() ;
+        PokemonMetadata pokemonMetadata = metadataProvider.getPokemonMetadata(index) ;
+        
+        double iv = (pokemonMetadata.getAttack() + pokemonMetadata.getDefense() + pokemonMetadata.getStamina()) / 45;
+        Pokemon pokemon = new Pokemon(index, pokemonMetadata.getName(), pokemonMetadata.getAttack(), pokemonMetadata.getDefense(),pokemonMetadata.getStamina(), cp, hp, dust, candy, 0);
+        return pokemon ;
     }
 }
