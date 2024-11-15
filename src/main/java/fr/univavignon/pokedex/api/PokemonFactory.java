af19ea8e55b22b5f669ad50409d9228d75e07549
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-11-15 10:12:03 +0100

    Fix error ?

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -1,17 +1,13 @@
 package fr.univavignon.pokedex.api;
-
 import java.util.Comparator;
 import java.util.List;
-import Random ; 
 
 public class PokemonFactory implements IPokemonFactory {
 
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-
         PokemonMetaDataProvider metadataProvider = new PokemonMetaDataProvider() ;
         PokemonMetadata pokemonMetadata = metadataProvider.getPokemonMetadata(index) ;
-        
         double iv = (pokemonMetadata.getAttack() + pokemonMetadata.getDefense() + pokemonMetadata.getStamina()) / 45;
         Pokemon pokemon = new Pokemon(index, pokemonMetadata.getName(), pokemonMetadata.getAttack(), pokemonMetadata.getDefense(),pokemonMetadata.getStamina(), cp, hp, dust, candy, 0);
         return pokemon ;
