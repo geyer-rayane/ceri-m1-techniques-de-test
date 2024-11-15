c64e7e73551ce2272d26fe38185c1a528fcc6813
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-11-15 11:21:19 +0100

    Remove

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryWithClassTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryWithClassTest.java
@@ -19,28 +19,4 @@ import static org.mockito.Mockito.*;
 
 public class IPokemonTrainerFactoryWithClassTest {
     
-    private IPokemonTrainerFactory pokemonTrainerFactory;
-    private IPokedexFactory pokedexFactory;
-    private IPokedex pokedex;
-    private PokemonTrainer pokemonTrainer;
-
-    private String name = "Rayane";
-    private Team team = Team.MYSTIC;
-
-    @Before
-    public void setup() {
-        pokedexFactory = new PokedexFactoryImpl();
-        pokedex = pokedexFactory.createPokedex();
-        pokemonTrainerFactory = new PokemonTrainerFactoryImpl();
-        pokemonTrainer = new PokemonTrainer(name, team, pokedex);
-    }
-
-    @Test
-    public void testCreateTrainer() {
-        PokemonTrainer newPokemonTrainer = pokemonTrainerFactory.createTrainer(name, team, pokedexFactory);
-        assertNotNull(newPokemonTrainer);
-        assertEquals(name, newPokemonTrainer.getName());
-        assertEquals(team, newPokemonTrainer.getTeam());
-        assertSame(pokedex, newPokemonTrainer.getPokedex());
-    }
 }
