c068ba69b170ae4899d497c82b1f9fef6971e5b3
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-11-15 10:59:30 +0100

    Tests on PokemonFactory

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryWithClassTest.java
@@ -0,0 +1,30 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IPokemonFactoryWithClassTest {
+
+    private PokemonFactory pokemonFactory;
+    private Pokemon expectedPokemon;
+    private Pokemon createdPokemon;
+
+    @Before
+    public void setUp() {
+        pokemonFactory = new PokemonFactory(); 
+        expectedPokemon = new Pokemon(0, 126, 126, 90, 4); 
+    }
+
+    @Test
+    public void testCreatePokemon() {
+        createdPokemon = pokemonFactory.createPokemon(0, 126, 126, 90, 4);
+        assertNotNull(createdPokemon);
+        assertEquals(expectedPokemon.getIndex(), createdPokemon.getIndex());
+        assertEquals(expectedPokemon.getAttack(), createdPokemon.getAttack());
+        assertEquals(expectedPokemon.getDefense(), createdPokemon.getDefense());
+        assertEquals(expectedPokemon.getStamina(), createdPokemon.getStamina());
+        assertEquals(expectedPokemon.getCandy(), createdPokemon.getCandy());
+    }
+}
