1b4286e0baeb25d0f6d4d32bf1c1a544a7cef5df
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-11-15 11:17:48 +0100

    Add import

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryWithClassTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryWithClassTest.java
@@ -1,3 +1,22 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+ 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Arrays;
+
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 public class IPokemonTrainerFactoryWithClassTest {
     
     private IPokemonTrainerFactory pokemonTrainerFactory;
