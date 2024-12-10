5b1e5351ca5de84a19203abfbaa398bdc3dc0524
rayanegeyer <rayane.geyer@alumni.univ-avignon.fr>
2024-12-10 16:25:00 +0100

    Fin TP6

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 [![CircleCI](https://dl.circleci.com/status-badge/img/gh/geyer-rayane/ceri-m1-techniques-de-test/tree/master.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/geyer-rayane/ceri-m1-techniques-de-test/tree/master)
 [![codecov](https://codecov.io/github/geyer-rayane/ceri-m1-techniques-de-test/graph/badge.svg?token=SMJIHS7Y8H)](https://codecov.io/github/geyer-rayane/ceri-m1-techniques-de-test)
-![Checkstyle](target/site/badges/checkstyle-result.svg)
+![Checkstyle](https://img.shields.io/badge/Checkstyle-Passed-brightgreen)
 
 # Documentation du Projet : :book: 
 
--- src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
@@ -3,27 +3,43 @@ package fr.univavignon.pokedex.api;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class IRocketPokemonFactoryTest {
 
-    private RocketPokemonFactory  rocketPokemonFactory;
-    private Pokemon createdPokemon;
+    private RocketPokemonFactory pokemonFactory;
 
     @Before
     public void setUp() {
-        rocketPokemonFactory = new RocketPokemonFactory();
+        pokemonFactory = new RocketPokemonFactory();
     }
 
     @Test
     public void testCreatePokemon() {
-        createdPokemon = rocketPokemonFactory.createPokemon(0, 126, 126, 90, 4);
-        assertEquals(0, createdPokemon.getIndex());
-        assertEquals(126, createdPokemon.getAttack());
-        assertEquals(126, createdPokemon.getDefense());
-        assertEquals(90, createdPokemon.getStamina());
-        assertEquals(4, createdPokemon.getCandy());
+        Pokemon pokemon = pokemonFactory.createPokemon(0, 500, 60, 4000, 3);
+        assertEquals(0, pokemon.getIndex());
+        assertEquals(500, pokemon.getCp());
     }
-    
 
+    @Test
+    public void testCreatePokemonWithIndex() {
+        Pokemon pokemon = pokemonFactory.createPokemon(133, 1000, 120, 5000, 10);
+        assertEquals(133, pokemon.getIndex());
+        assertEquals(1000, pokemon.getCp());
+        assertEquals(120, pokemon.getHp());
+        assertEquals(5000, pokemon.getDust());
+        assertEquals(10, pokemon.getCandy());
+    }
+
+    @Test
+    public void testCreatePokemonInvalidIndex() {
+        Pokemon pokemon = pokemonFactory.createPokemon(-1, 500, 60, 4000, 3);
+        assertEquals("Ash's Pikachu", pokemon.getName());
+    }
+
+    @Test
+    public void testCreatePokemonWithIndexToHigh() {
+        Pokemon pokemon = pokemonFactory.createPokemon(155, 500, 60, 4000, 3);
+        assertEquals("MISSINGNO", pokemon.getName());
+    }
 }
